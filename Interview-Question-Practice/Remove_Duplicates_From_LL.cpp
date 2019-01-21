282146f40ed7fa5576354afcab31799338b78451
KenRatliff <kjhsfjk@yahoo.com>
2019-01-21 13:44:57 -0600

    solution for removing duplicates from LL

--- Interview-Question-Practice/Remove_Duplicates_From_LL.cpp
+++ Interview-Question-Practice/Remove_Duplicates_From_LL.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include <set>
-#include "LinkedList.h"
-
+#include <list>
 using namespace std;
 
-int main()
-{
+int main() {
 
+	set<char> charSet;
+	list<char> str({ 'a', 'a', 'b', 'b', 'b', 'c', 'd', 'e', 'e', 'f', 'g', 'h', 'h' });
+
+	auto listItr = str.begin();
+
+	while (listItr != str.end()) {
+
+		auto setItr = charSet.insert(*listItr);
+
+		if (setItr.second == false) {
+
+			auto duplicate = listItr;
+			++listItr;
+			str.erase(duplicate);
+		}
+		else
+			++listItr;
+	}
+
+	for (auto itr = str.begin(); itr != str.end(); ++itr)
+		cout << *itr << " ";
 
 	cout << "\n\n";
 	cin.get();
