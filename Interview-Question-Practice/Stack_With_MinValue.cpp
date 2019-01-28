547eaab2879b7d37f456521e5012c79ae2042b77
KenRatliff <kjhsfjk@yahoo.com>
2019-01-28 02:38:36 -0600

    solved Stack_With_MinValue

--- Interview-Question-Practice/Stack_With_MinValue.cpp
+++ Interview-Question-Practice/Stack_With_MinValue.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+/*
+Implementation of a typical stack that also keeps track of the minimum value in the stack.
+Each node has a minValue. The values of the subsequent nodes pushed onto the stack are 
+compared to the minValue of the previous node. If value <= minValue then value will be
+stored as the minValue of the node being pushed onto the stack. This ensures that you don't
+have to search for the minValue everytime you pop from the stack.
+--------------------------------------------------------------------------------------------
+The operations of the stack, push, pop, isEmpty, and getMin are all O(1) time.
+*/
+
 class Stack {
 
 	struct Node {
@@ -16,18 +26,39 @@ class Stack {
 	Node* _top;
 
 public:
-	Stack() { _top = NULL; }
+	Stack(): _top(NULL) {}
 	~Stack() { destroyStack(); }
 	bool isEmpty() const;
 	void push(int value);
-	int pop(int &num);
-	int getMin(int &num) const;
+	int pop();
+	int getMin() const;
 	void destroyStack();
 };
 
 int main() {
 
-	cout << "\n\n";
+	Stack myStack;
+
+	myStack.push(3);
+	myStack.push(8);
+	myStack.push(2);
+	myStack.push(7);
+	myStack.push(11);
+	myStack.push(1);
+	myStack.push(2);
+	myStack.push(5);
+	myStack.push(10);
+	myStack.push(9);
+
+	int num = 0;
+
+	while (!myStack.isEmpty()) {
+
+		cout << "min:"<< myStack.getMin() << "  ";
+		cout << myStack.pop() << endl;
+	}
+
+	cout << "\n";
 	cin.get();
 
 	return 0;
@@ -45,13 +76,15 @@ void Stack::push(int value) {
 
 	if (isEmpty())
 		_top = new Node(value, value, _top);
-	else if (value < _top->_minValue)
+	else if (value <= _top->_minValue)
 		_top = new Node(value, value, _top);
 	else
 		_top = new Node(value, _top->_minValue, _top);
 }
 
-int Stack::pop(int &num) {
+int Stack::pop() {
+
+	int num;
 
 	if (isEmpty())
 		cout << "\n\nstack is empty\n\n";
@@ -66,7 +99,7 @@ int Stack::pop(int &num) {
 	return num;
 }
 
-int Stack::getMin(int &num) const {
+int Stack::getMin() const {
 
 	if (isEmpty())
 		cout << "\n\nstack is empty\n\n";
